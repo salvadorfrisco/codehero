898d92f58f3060f0a55b7dcbda05242606486e90
Salvador Frisco <salvadorfrisco70@gmail.com>
2024-03-21 23:45:40 -0300

    refactor: improving state management

--- lib/features/heroes/presenter/pages/home_page.dart
+++ lib/features/heroes/presenter/pages/home_page.dart
@@ -3,17 +3,27 @@ import 'package:hero/features/heroes/presenter/providers/heroes_provider.dart';
 import 'package:hero/features/heroes/presenter/widgets/hero_list.dart';
 import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late HeroesProvider _provider;
+
+  @override
+  void initState() {
+    super.initState();
+    _provider = HeroesProvider();
+    _provider.loadHeroes();
+  }
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) {
-        final provider = HeroesProvider();
-        provider.loadHeroes();
-        return provider;
-      },
+      create: (_) => _provider,
       child: _HomePage(),
     );
   }
@@ -22,8 +32,6 @@ class HomePage extends StatelessWidget {
 class _HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<HeroesProvider>(context);
-
     return GestureDetector(
         behavior: HitTestBehavior.translucent,
         onTap: () {
@@ -80,17 +88,20 @@ class _HomePage extends StatelessWidget {
                               style: Theme.of(context).textTheme.titleSmall),
                           SizedBox(
                             height: 40,
-                            child: TextFormField(
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                contentPadding:
-                                    EdgeInsets.symmetric(horizontal: 8.0),
-                              ),
-                              textAlignVertical: TextAlignVertical.center,
-                              onChanged: (value) {
-                                provider.setFilterName(value);
-                              },
-                            ),
+                            child: Consumer<HeroesProvider>(
+                                builder: (context, heroes, child) {
+                              return TextFormField(
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  contentPadding:
+                                      EdgeInsets.symmetric(horizontal: 8.0),
+                                ),
+                                textAlignVertical: TextAlignVertical.center,
+                                onChanged: (value) {
+                                  heroes.setFilterName(value);
+                                },
+                              );
+                            }),
                           ),
                           const SizedBox(height: 12),
                         ],
@@ -98,210 +109,212 @@ class _HomePage extends StatelessWidget {
                     ),
                   ),
                   body: constraints.maxWidth > 600
-                      ? _buildWebLayout(context, provider)
-                      : _buildMobileLayout(context, provider));
+                      ? _buildWebLayout()
+                      : _buildMobileLayout());
             },
           ),
         ));
   }
 
-  Widget _buildWebLayout(BuildContext context, HeroesProvider provider) {
-    return Column(
-      children: <Widget>[
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 24),
-          height: 40,
-          alignment: Alignment.center,
-          child: Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Container(
-                  color: Theme.of(context).primaryColor,
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Text(
-                      'Personagem',
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(width: 8),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  color: Theme.of(context).primaryColor,
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Text(
-                      'Séries',
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(width: 8),
-              Expanded(
-                flex: 2,
-                child: Container(
-                  color: Theme.of(context).primaryColor,
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Text(
-                      'Eventos',
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-          child: provider.isLoading
-              ? Center(
-                  child: CircularProgressIndicator(
-                  color: Theme.of(context).primaryColor,
-                ))
-              : provider.heroesList.isNotEmpty
-                  ? Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 24),
-                      child: HeroList(
-                        heroesList: provider.heroesList,
-                        web: true,
-                      ),
-                    )
-                  : Center(
-                      child: Text(
-                      'Ops! não há personagens...',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    )),
-        ),
-        const SizedBox(height: 18),
-        Padding(
-          padding: const EdgeInsets.only(top: 18, bottom: 24),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                onPressed: provider.previousPage,
-                icon: const Icon(Icons.arrow_left),
-                iconSize: 48,
-                color: provider.currentPage > 0
-                    ? Theme.of(context).primaryColor
-                    : Colors.black26,
-              ),
-              const SizedBox(width: 16),
-              ..._buildPageButtons(context, provider),
-              const SizedBox(width: 16),
-              IconButton(
-                onPressed: provider.nextPage,
-                icon: const Icon(Icons.arrow_right),
-                iconSize: 48,
-                color:
-                    provider.currentPage + 1 < (provider.totalHeroes / 4).ceil()
-                        ? Theme.of(context).primaryColor
-                        : Colors.black26,
-              ),
-            ],
-          ),
-        ),
-        Container(
-          color: Theme.of(context).primaryColor,
-          height: 16,
-        ),
-      ],
-    );
-  }
-
-  Widget _buildMobileLayout(BuildContext context, HeroesProvider provider) {
-    return Column(
-      children: <Widget>[
-        Container(
-            color: Theme.of(context).primaryColor,
+  Widget _buildWebLayout() {
+    return Consumer<HeroesProvider>(builder: (context, heroes, child) {
+      return Column(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 24),
             height: 40,
             alignment: Alignment.center,
             child: Row(
               children: [
-                const SizedBox(
-                  width: 96,
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Theme.of(context).primaryColor,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
+                      child: Text(
+                        'Personagem',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ),
+                  ),
                 ),
-                Text(
-                  'Nome',
-                  style: Theme.of(context).textTheme.titleMedium,
+                const SizedBox(width: 8),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Theme.of(context).primaryColor,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
+                      child: Text(
+                        'Séries',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Theme.of(context).primaryColor,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
+                      child: Text(
+                        'Eventos',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ),
+                  ),
                 ),
               ],
-            )),
-        Expanded(
-          child: provider.isLoading
-              ? Center(
-                  child: CircularProgressIndicator(
-                  color: Theme.of(context).primaryColor,
-                ))
-              : provider.heroesList.isNotEmpty
-                  ? HeroList(
-                      heroesList: provider.heroesList,
-                      web: false,
-                    )
-                  : Center(
-                      child: Text(
-                      'Ops! não há personagens...',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    )),
-        ),
-        const SizedBox(height: 18),
-        Padding(
-          padding: const EdgeInsets.only(top: 18, bottom: 24),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                onPressed: provider.previousPage,
-                icon: const Icon(Icons.arrow_left),
-                iconSize: 48,
-                color: provider.currentPage > 0
-                    ? Theme.of(context).primaryColor
-                    : Colors.black26,
-              ),
-              const SizedBox(width: 16),
-              ..._buildPageButtons(context, provider),
-              const SizedBox(width: 16),
-              IconButton(
-                onPressed: provider.nextPage,
-                icon: const Icon(Icons.arrow_right),
-                iconSize: 48,
-                color:
-                    provider.currentPage + 1 < (provider.totalHeroes / 4).ceil()
-                        ? Theme.of(context).primaryColor
-                        : Colors.black26,
-              ),
-            ],
+            ),
           ),
-        ),
-        Container(
-          color: Theme.of(context).primaryColor,
-          height: 16,
-        ),
-      ],
-    );
+          Expanded(
+            child: heroes.isLoading
+                ? Center(
+                    child: CircularProgressIndicator(
+                    color: Theme.of(context).primaryColor,
+                  ))
+                : heroes.heroesList.isNotEmpty
+                    ? Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        child: HeroList(
+                          heroesList: heroes.heroesList,
+                          web: true,
+                        ),
+                      )
+                    : Center(
+                        child: Text(
+                        'Ops! não há personagens...',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      )),
+          ),
+          const SizedBox(height: 18),
+          Padding(
+            padding: const EdgeInsets.only(top: 18, bottom: 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  onPressed: heroes.previousPage,
+                  icon: const Icon(Icons.arrow_left),
+                  iconSize: 48,
+                  color: heroes.currentPage > 0
+                      ? Theme.of(context).primaryColor
+                      : Colors.black26,
+                ),
+                const SizedBox(width: 16),
+                ..._buildPageButtons(context, heroes),
+                const SizedBox(width: 16),
+                IconButton(
+                  onPressed: heroes.nextPage,
+                  icon: const Icon(Icons.arrow_right),
+                  iconSize: 48,
+                  color:
+                      heroes.currentPage + 1 < (heroes.totalHeroes / 4).ceil()
+                          ? Theme.of(context).primaryColor
+                          : Colors.black26,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: Theme.of(context).primaryColor,
+            height: 16,
+          ),
+        ],
+      );
+    });
   }
 
-  List<Widget> _buildPageButtons(
-      BuildContext context, HeroesProvider provider) {
+  Widget _buildMobileLayout() {
+    return Consumer<HeroesProvider>(builder: (context, heroes, child) {
+      return Column(
+        children: <Widget>[
+          Container(
+              color: Theme.of(context).primaryColor,
+              height: 40,
+              alignment: Alignment.center,
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 96,
+                  ),
+                  Text(
+                    'Nome',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ],
+              )),
+          Expanded(
+            child: heroes.isLoading
+                ? Center(
+                    child: CircularProgressIndicator(
+                    color: Theme.of(context).primaryColor,
+                  ))
+                : heroes.heroesList.isNotEmpty
+                    ? HeroList(
+                        heroesList: heroes.heroesList,
+                        web: false,
+                      )
+                    : Center(
+                        child: Text(
+                        'Ops! não há personagens...',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      )),
+          ),
+          const SizedBox(height: 18),
+          Padding(
+            padding: const EdgeInsets.only(top: 18, bottom: 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  onPressed: heroes.previousPage,
+                  icon: const Icon(Icons.arrow_left),
+                  iconSize: 48,
+                  color: heroes.currentPage > 0
+                      ? Theme.of(context).primaryColor
+                      : Colors.black26,
+                ),
+                const SizedBox(width: 16),
+                ..._buildPageButtons(context, heroes),
+                const SizedBox(width: 16),
+                IconButton(
+                  onPressed: heroes.nextPage,
+                  icon: const Icon(Icons.arrow_right),
+                  iconSize: 48,
+                  color:
+                      heroes.currentPage + 1 < (heroes.totalHeroes / 4).ceil()
+                          ? Theme.of(context).primaryColor
+                          : Colors.black26,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: Theme.of(context).primaryColor,
+            height: 16,
+          ),
+        ],
+      );
+    });
+  }
+
+  List<Widget> _buildPageButtons(BuildContext context, HeroesProvider heroes) {
     final List<Widget> buttons = [];
-    final int totalPages = (provider.totalHeroes / 4).ceil();
-    final int startPage = provider.currentPage > 0
-        ? provider.currentPage - 1
-        : provider.currentPage;
+    final int totalPages = (heroes.totalHeroes / 4).ceil();
+    final int startPage =
+        heroes.currentPage > 0 ? heroes.currentPage - 1 : heroes.currentPage;
     final int endPage =
         startPage + 2 < totalPages ? startPage + 2 : totalPages - 1;
 
     for (int i = startPage; i <= endPage; i++) {
-      buttons.add(_buildPageButton(context, provider, i + 1));
+      buttons.add(_buildPageButton(context, heroes, i + 1));
       if (i < endPage) {
         buttons.add(const SizedBox(width: 16));
       }
@@ -311,10 +324,10 @@ class _HomePage extends StatelessWidget {
   }
 
   Widget _buildPageButton(
-      BuildContext context, HeroesProvider provider, int page) {
-    final isActive = page == provider.currentPage + 1;
+      BuildContext context, HeroesProvider heroes, int page) {
+    final isActive = page == heroes.currentPage + 1;
     return GestureDetector(
-      onTap: () => provider.goToPage(page),
+      onTap: () => heroes.goToPage(page),
       child: Container(
         padding: const EdgeInsets.all(8),
         decoration: BoxDecoration(
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hero/features/heroes/presenter/pages/home_page.dart';
-import 'package:hero/features/heroes/presenter/providers/heroes_provider.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,57 +10,57 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => HeroesProvider()),
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: const Color(0xFFD42026),
-          primaryColorDark: const Color(0xFF4E4E4E),
-          primaryColorLight: const Color(0xFFFFFFFF),
-          textTheme: const TextTheme(
-              bodyLarge: TextStyle(
-                fontSize: 21,
-                color: Color(0xFF4E4E4E),
-              ),
-              bodyMedium: TextStyle(
-                color: Color(0x00000fff),
-                fontSize: 16,
-                fontFamily: 'Roboto-Regular',
-                letterSpacing: 0,
-                height: 1.2,
-              ),
-              titleLarge: TextStyle(
-                color: Color(0xFFD42026),
-                fontSize: 16,
-                fontFamily: 'Roboto-Black',
-                fontWeight: FontWeight.bold,
-                letterSpacing: 0,
-                height: 1.2,
-              ),
-              titleMedium: TextStyle(
-                color: Color(0xFFFFFFFF),
-                fontSize: 16,
-                fontFamily: 'Roboto-Regular',
-                fontWeight: FontWeight.normal,
-                letterSpacing: 0,
-                height: 1.2,
-              ),
-              titleSmall: TextStyle(
-                color: Color(0xFFD42026),
-                fontSize: 16,
-                fontFamily: 'Roboto-Light',
-                fontWeight: FontWeight.normal,
-                letterSpacing: 0,
-                height: 1.2,
-              )),
-          useMaterial3: true,
+    return MaterialApp(
+      title: 'Code Hero - Salvador Frisco',
+      debugShowCheckedModeBanner: false,
+      theme: _getTheme(),
+      home: const HomePage(),
+    );
+  }
+
+  ThemeData _getTheme() {
+    return ThemeData(
+      primaryColor: const Color(0xFFD42026),
+      primaryColorDark: const Color(0xFF4E4E4E),
+      primaryColorLight: const Color(0xFFFFFFFF),
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(
+          fontSize: 21,
+          color: Color(0xFF4E4E4E),
         ),
-        home: const HomePage(),
+        bodyMedium: TextStyle(
+          color: Color(0x00000fff),
+          fontSize: 16,
+          fontFamily: 'Roboto-Regular',
+          letterSpacing: 0,
+          height: 1.2,
+        ),
+        titleLarge: TextStyle(
+          color: Color(0xFFD42026),
+          fontSize: 16,
+          fontFamily: 'Roboto-Black',
+          fontWeight: FontWeight.bold,
+          letterSpacing: 0,
+          height: 1.2,
+        ),
+        titleMedium: TextStyle(
+          color: Color(0xFFFFFFFF),
+          fontSize: 16,
+          fontFamily: 'Roboto-Regular',
+          fontWeight: FontWeight.normal,
+          letterSpacing: 0,
+          height: 1.2,
+        ),
+        titleSmall: TextStyle(
+          color: Color(0xFFD42026),
+          fontSize: 16,
+          fontFamily: 'Roboto-Light',
+          fontWeight: FontWeight.normal,
+          letterSpacing: 0,
+          height: 1.2,
+        ),
       ),
+      useMaterial3: true,
     );
   }
 }
